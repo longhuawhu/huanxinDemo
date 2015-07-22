a0e3fcd49fbb3d2eff23b7385cba8682b2c78ced
longhuawhu <longhua1949hm@163.com>
2015-07-22 18:02:59 +0800

    添加环信SDK

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// EaseMobSDK
+#define COCOAPODS_POD_AVAILABLE_EaseMobSDK
+#define COCOAPODS_VERSION_MAJOR_EaseMobSDK 2
+#define COCOAPODS_VERSION_MINOR_EaseMobSDK 1
+#define COCOAPODS_VERSION_PATCH_EaseMobSDK 8
+
